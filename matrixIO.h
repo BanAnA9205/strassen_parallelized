0bf93e8599762a19fb7e9012e5bc365a9cb018ef
SquaringHard <umr091105@gmail.com>
2025-10-08 17:09:54 +0700

    readMatrixFromBinaryFile has out parameter instead of returning value

--- matrixIO.h
+++ matrixIO.h
@@ -4,11 +4,11 @@
 #include <fstream>
 #include <cstdint>
 #include <random>
-#include <iostream>
+#include <print>
 #include <string>
 #include <array>
 #include <tuple>
-#define NDEBUG
+
 
 template<typename T, size_t N>
 using Matrix = std::array<std::array<T, N>, N>;
@@ -22,25 +22,17 @@ void writeRandomMatrixToBinaryFile(std::string_view filename, uint32_t dim, floa
 
 	static std::mt19937 gen(std::random_device{}());
 	static std::uniform_real_distribution<float> dist(lower, upper);
-	for (uint32_t i = 0; i < dim; ++i) {
-		for (uint32_t j = 0; j < dim; ++j) {
-			float value = dist(gen);
-#ifndef NDEBUG
-			std::cout << value << ' ';
-#endif
-			outFile.write(reinterpret_cast<const char*>(&value), sizeof(value));
-		}
-#ifndef NDEBUG
-		std::cout << std::endl;
-#endif
+	for (uint32_t i = 0; i < dim * dim; ++i) {
+		float value = dist(gen);
+		outFile.write(reinterpret_cast<const char*>(&value), sizeof(value));
 	}
 
-	std::cout << "Matrix written to " << filename << std::endl;
+	std::println("Matrix written to {}", filename);
 }
 
-// Read a matrix from a binary file and return it
+// Read a matrix from a binary file
 template <uint32_t dim>
-auto readMatrixFromBinaryFile(std::string_view filename) {
+void readMatrixFromBinaryFile(std::string_view filename, Matrix<float, dim>& matrix) {
 	std::ifstream inFile(filename.data(), std::ios::binary);
 	if (!inFile) throw std::ios_base::failure("Failed to open file for reading.");
 
@@ -50,26 +42,23 @@ auto readMatrixFromBinaryFile(std::string_view filename) {
 		throw std::runtime_error("Dimension mismatch: expected " + std::to_string(dim) + ", got " + std::to_string(in_dim));
 	}
 
-	Matrix<float, dim> matrix;
 	inFile.read(reinterpret_cast<char*>(&matrix[0][0]), static_cast<std::streamsize>(dim * dim * sizeof(float)));
-
-	return matrix;
 }
 
 
 
 //// Example usage
-//#include <print>
-//
 //int main() {
 //	const char* filename = "matrix.bin";
 //	constexpr uint32_t dim = 4; // dim must be known at compile time for readMatrixFromBinaryFile
 //	writeRandomMatrixToBinaryFile(filename, dim);
-//	Matrix<float, dim> matrix{ readMatrixFromBinaryFile<dim>(filename) };
+//
+//	Matrix<float, dim> matrix;
+//	readMatrixFromBinaryFile(filename, matrix);
+//
 //	for (const auto& row : matrix) {
 //		std::println("{}", row);
 //	}
 //}
 
 #endif // MATRIX_IO_H
-
